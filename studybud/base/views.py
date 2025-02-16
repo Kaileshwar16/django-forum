588fe81409cecd34a7328c9884fd2309f9c8ebb5
kaileshwar16 <kaileshwar2005@gmail.com>
2025-02-16 19:07:28 +0530

    recalling templating

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+rooms = [{"id":1,"name":"study python"},
+         {"id":2,"name":"study c++"},
+         {"id":3,"name":"learn pyhysics"},
+         {"id":4,"name":" learn go "}]
 def home(request):
-    return HttpResponse('home page')
+    context = {"rooms":rooms}
+    return render(request,'base/home.html',context)
+
+def room(request,pk):
+    context = None
+    for room in rooms:
+        if room['id'] == int(pk):
+            context = {"room":room}
+    return render(request,'base/room.html',context)
